afddf1416300b1af09b664d8367f4b6c44a88182
Evghenii Costin <evghenii.costin@uni-oldenburg.de>
2020-11-13 17:44:07 +0100

    slider variant 1 und 2 was added

--- js/script.js
+++ js/script.js
@@ -4,8 +4,7 @@ window.addEventListener('DOMContentLoaded', function () {
 
     let tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParent = document.querySelector('.tabheader__items'),
-        timerId;
+        tabsParent = document.querySelector('.tabheader__items');
 
     function hideTabContent() {
 
@@ -205,7 +204,7 @@ window.addEventListener('DOMContentLoaded', function () {
         return await res.json();
     };
 
-   /*  getResourse('http://localhost:3000/menu')
+    /*  getResourse('http://localhost:3000/menu')
         .then(data => {
             data.forEach(({
                 img,
@@ -219,7 +218,7 @@ window.addEventListener('DOMContentLoaded', function () {
         });
  */
 
-        axios.get('http://localhost:3000/menu')
+    axios.get('http://localhost:3000/menu')
         .then(data => {
             data.data.forEach(({
                 img,
@@ -341,9 +340,128 @@ window.addEventListener('DOMContentLoaded', function () {
         }, 4000);
     }
 
-         fetch('http://localhost:3000/menu')
-         .then(data => data.json())
-         .then(res => console.log(res));
+    fetch('http://localhost:3000/menu')
+        .then(data => data.json())
+        .then(res => console.log(res));
+
+    //Slider
+
+    const slides = document.querySelectorAll('.offer__slide'),
+        prev = document.querySelectorAll('.offer__slider-prev'),
+        next = document.querySelectorAll('.offer__slider-next'),
+        total = document.querySelector('#total'),
+        current = document.querySelector('#current'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        width = window.getComputedStyle(slidesWrapper).width,
+        slidesField = document.querySelector('.offer__slider-inner');
+    let slideIndex = 1,
+        offset = 0;
+
+
+
+    /*    showSlides(slideIndex);
+
+       if(slides.length < 10){
+          total.textContent = `0${slides.length}`;
+      } else {
+         total.textContent = slides.length;
+      }
+
+       function showSlides(n) {
+
+          if(n > slides.length){
+              slideIndex = 1;
+          }
+
+          if(n < 1){
+              slideIndex = slides.length;
+          }
+
+          slides.forEach(item =>item.style.display = 'none' )// скрываем слайды 
+          sliders[slideIndex -1].style.display = 'block'; // показываем нужный слайд 
+
+          if(slides.length < 10){
+              current.textContent = `0${slideIndex}`;
+          } else {
+              current.textContent = slideIndex;
+          }
+      }
+
+       function plusSlider(n) {
+         showSlides(slideIndex +=n);
+      }
+
+      prev.addEventListener('click', () => {
+          plusSlider(-1);
+      })
+
+      next.addEventListener('click', () => {
+          plusSlider(+1);
+      }) */
+
+
+    //Slider Alternativa 
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent =  `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent =  slideIndex;
+    }
+    
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    next.addEventListener('click', () => {
+        if (offset == (+width.slice(0, width.length - 2) * (slides.length - 1))) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2); 
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    });
+
+    prev.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent =  `0${slideIndex}`;
+        } else {
+            current.textContent =  slideIndex;
+        }
+    });
 
 
 });
